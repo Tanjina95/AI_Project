57a84997f9f76a6dc3692e722d7465d9b5057d4f
Tanjina95 <polytanjinaakter@gmail.com>
2021-08-16 00:06:25 +0600

    final commited

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ async function start() {
   })
 }
 
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America','Tanjina' ,'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
